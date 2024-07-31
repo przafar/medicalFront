06a430d8e9eccc57627594c00389ad48c0af7b2a
ra <ra@gmail.com>
2024-07-31 13:37:39 +0500

    update project

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable perfectionist/sort-imports */
+import { useEffect } from 'react';
 import 'src/global.css';
 
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
@@ -8,7 +9,15 @@ import ThemeProvider from 'src/theme';
 
 export default function App() {
 
-
+  useEffect(() => {
+    fetch('http://192.168.110.136:3001/api/patients')
+      .then((res) => {
+        console.log('Список пациентов загружен', res.headers);
+      })
+      .catch((err) => {
+        alert('Ошибка при загрузке пациентов:', err);
+      })
+  })
 
   useScrollToTop();
 
