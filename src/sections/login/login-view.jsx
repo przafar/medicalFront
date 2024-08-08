38ca6c0f13d7ba0f92fb01ceb5636f149f2ab3a7
ra <ra@gmail.com>
2024-08-08 11:47:35 +0500

    modal

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -45,7 +45,7 @@ export default function LoginView() {
       })
       .catch((error) => {
         console.log('ошибка', error);
-        setLoading(false);
+        setLoading(false);  
       });
   };
 
@@ -54,7 +54,7 @@ export default function LoginView() {
   const [passwords, setPassword] = useState('');
   const [loading, setLoading] = useState(true)
 
-  if(loading) {
+  if(!loading) {
     console.log('loading');
   }
 
@@ -130,78 +130,4 @@ export default function LoginView() {
       </Stack>
     </Box>
   );
-
-  // loading ? <h1>Loading</h1> : 
-  //   const renderForm = (
-  //   <>
-  //     <Stack spacing={3}>
-  //       <TextField
-  //         onChange={(e) => setEmail(e.target.value)}
-  //         value={email}
-  //         name="email"
-  //         label="Email address"
-  //       />
-
-  //       <TextField
-  //         onChange={(e) => setPassword(e.target.value)}
-  //         value={passwords}
-  //         name="password"
-  //         label="Password"
-  //         type={showPassword ? 'text' : 'password'}
-  //         InputProps={{
-  //           endAdornment: (
-  //             <InputAdornment position="end">
-  //               <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-  //                 <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-  //               </IconButton>
-  //             </InputAdornment>
-  //           ),
-  //         }}
-  //       />
-  //     </Stack>
-  //     <LoadingButton
-  //       fullWidth
-  //       size="large"
-  //       type="submit"
-  //       variant="contained"
-  //       color="inherit"
-  //       onClick={handleSubmit}
-  //     >
-  //       Login
-  //     </LoadingButton>
-  //   </>
-  // );
-
-  // return (
-  //   <Box
-  //     sx={{
-  //       ...bgGradient({
-  //         color: alpha(theme.palette.background.default, 0.9),
-  //         imgUrl: '/assets/background/overlay_4.jpg',
-  //       }),
-  //       height: 1,
-  //     }}
-  //   >
-  //     <Logo
-  //       sx={{
-  //         position: 'fixed',
-  //         top: { xs: 16, md: 24 },
-  //         left: { xs: 16, md: 24 },
-  //       }}
-  //     />
-
-  //     <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
-  //       <Card
-  //         sx={{
-  //           p: 5,
-  //           width: 1,
-  //           maxWidth: 420,
-  //         }}
-  //       >
-  //         <Typography variant="h4">Sign in </Typography>
-  //         {renderForm}
-  //       </Card>
-  //     </Stack>
-  //   </Box>
-  // );
 }
--- src/sections/user/view/user-view.jsx
+++ src/sections/user/view/user-view.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
@@ -21,11 +22,14 @@ import TableEmptyRows from '../table-empty-rows';
 import UserTableToolbar from '../user-table-toolbar';
 import { emptyRows, applyFilter, getComparator } from '../utils';
 
+import './Modal.scss';
+import close from '../../../../public/assets/close.svg'
+
 import axios from 'axios';
 
 // ----------------------------------------------------------------------
 
-export default function   UserPage() {
+export default function UserPage() {
   const [page, setPage] = useState(0);
 
   const [order, setOrder] = useState('asc');
@@ -66,81 +70,143 @@ export default function   UserPage() {
   });
 
   const notFound = !dataFiltered.length && !!filterName;
-  const [patients, setPatients] = useState([])
-  const [error, setError] = useState('')
+  const [patients, setPatients] = useState([]);
+  const [error, setError] = useState('');
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
-    axios.get('http://192.168.110.136:3001/api/patients', {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
+    axios
+      .get('http://192.168.110.136:3001/api/patients', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
       .then((res) => {
-        setPatients(res.data.data)
+        setPatients(res.data.data);
       })
       .catch((err) => {
         setError(err.message);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
-    <Container>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Patients</Typography>
+    <>
+      <div className={modal ? 'modal active' : 'modal'}>
+        <div className="modal__wrapper">
+          <h1 className="modal__wrapper-title">Add user</h1>
+          <img onClick={() => setModal(false)} className='modal__wrapper-img' src={close} alt="" />
+          <div className="modal__wrapper-logo">
+            <div className="modal__wrapper-logo-icon"></div>
+            <button className="modal__wrapper-logo-button">Add image</button>
+          </div>
+          <div className="modal__wrapper-inputs">
+            <div className='modal__wrapper-inputs-input'>
+              <p>full name</p>
+              <input type="text" />
+            </div>
+            <div className='modal__wrapper-inputs-input'>
+              <p>username</p>
+              <input type="text" />
+            </div>
+            <div className='modal__wrapper-inputs-input'>
+              <p>email</p>
+              <input type="text" />
+            </div>
+            <div className='modal__wrapper-inputs-input'>
+              <p>projects</p>
+              <input type="text" />
+            </div>
+            <select className='modal__wrapper-inputs-section'>
+              <option value="">User</option>
+              <option value="">Admin</option>
+            </select>
+            <div className='modal__wrapper-inputs-radio'>
+              <input type="checkbox" />
+              <p>Active</p>
+            </div>
+          </div>
+          <div className='modal__wrapper-textarea'>
+            <p>notes</p>
+            <textarea name="" id=""></textarea>
+          </div>
+          <div className='modal__wrapper-buttons'>
+            <button>Cancel</button>
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
+      <Container>
+        {patients.length === 0 ? (
+          <div>
+            <h1 className='text-6xl text-blue-400 cursor-default'>Вы не зарегистрированы</h1>
+            <Link to='/login' className='bg-blue-500 text-white text-lg py-2 px-5 rounded-md cursor-pointer duration-200 hover:bg-blue-600'><button className='mt-8'>Войти</button></Link>
+          </div>
+        ) : (
+          <>
+            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+              <Typography variant="h4">Patients</Typography>
 
-        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
-          New User
-        </Button>
-      </Stack>
+              <Button
+                variant="contained"
+                color="inherit"
+                startIcon={<Iconify icon="eva:plus-fill" />}
+                onClick={() => setModal(true)}
+              >
+                New User
+              </Button>
+            </Stack>
 
-      <Card>
-        <UserTableToolbar
-          numSelected={selected.length}
-          filterName={filterName}
-          onFilterName={handleFilterByName}
-        />
-
-        <Scrollbar>
-          <TableContainer sx={{ overflow: 'unset' }}>
-            <Table sx={{ minWidth: 800 }}>
-              <UserTableHead
-                order={order}
-                orderBy={orderBy}
-                rowCount={users.length}
+            <Card>
+              <UserTableToolbar
                 numSelected={selected.length}
-                onRequestSort={handleSort}
-                onSelectAllClick={handleSelectAllClick}
-                headLabel={[
-                  { id: 'name', label: 'Name' },
-                  { id: 'company', label: 'Last Name' },
-                  { id: 'role', label: 'Phone' },
-                  { id: 'isVerified', label: 'Verified', align: 'center' },
-                  { id: 'status', label: 'Middle Name' },
-                  { id: '' },
-                ]}
+                filterName={filterName}
+                onFilterName={handleFilterByName}
               />
-              <TableBody>
-                {patients.map((row) => (
-                    <UserTableRow
-                      key={row.id}
-                      name={row.first_name}
-                      role={row.phone_number}
-                      status={row.middle_name}
-                      company={row.last_name}
-                      avatarUrl={row.avatarUrl}
-                    />
-                ))}
-                <TableEmptyRows
-                  height={77}
-                  emptyRows={emptyRows(page, rowsPerPage, users.length)}
-                />
 
-                {notFound && <TableNoData query={filterName} />}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Scrollbar>
-      </Card>
-    </Container>
+              <Scrollbar>
+                <TableContainer sx={{ overflow: 'unset' }}>
+                  <Table sx={{ minWidth: 800 }}>
+                    <UserTableHead
+                      order={order}
+                      orderBy={orderBy}
+                      rowCount={users.length}
+                      numSelected={selected.length}
+                      onRequestSort={handleSort}
+                      onSelectAllClick={handleSelectAllClick}
+                      headLabel={[
+                        { id: 'name', label: 'Name' },
+                        { id: 'company', label: 'Last Name' },
+                        { id: 'role', label: 'Phone' },
+                        { id: 'isVerified', label: 'Verified', align: 'center' },
+                        { id: 'status', label: 'Middle Name' },
+                        { id: '' },
+                      ]}
+                    />
+                    <TableBody>
+                      {patients.map((row) => (
+                        <UserTableRow
+                          key={row.id}
+                          name={row.first_name}
+                          role={row.phone_number}
+                          status={row.middle_name}
+                          company={row.last_name}
+                          avatarUrl={row.avatarUrl}
+                        />
+                      ))}
+                      <TableEmptyRows
+                        height={77}
+                        emptyRows={emptyRows(page, rowsPerPage, users.length)}
+                      />
+
+                      {notFound && <TableNoData query={filterName} />}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Scrollbar>
+            </Card>
+          </>
+        )}
+      </Container>
+    </>
   );
 }
